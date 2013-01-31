d8316b8106ab41c95b6e6147584462fe16ea56ad
liantics <liantics.antics@gmail.com>
2013-01-31 18:12:35 -0500

    Initial commit one month rails class

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '1a0bf85f60ca92f774ba457d7bd206ae28604aa16576516c727517fc6d478bc66bb26bfd8bd01c1d09c7085651c82ac43c768c1f3eec5926db0de4b5eef1e716'
