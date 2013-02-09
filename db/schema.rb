927e35078df0ce66ec6fca4c23ce161edc1bf056
liantics <liantics.antics@gmail.com>
2013-02-08 23:16:54 -0500

    added user associations to pins, added a pin while logged in, added some validation on add - user and description must exist.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130209010922) do
+ActiveRecord::Schema.define(:version => 20130209025129) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -50,7 +50,10 @@ ActiveRecord::Schema.define(:version => 20130209010922) do
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
