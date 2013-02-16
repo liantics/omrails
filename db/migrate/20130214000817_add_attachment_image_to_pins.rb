32cc81617527e6a2cc932771523512539797dd90
liantics <liantics.antics@gmail.com>
2013-02-15 23:40:50 -0500

    changed t.attachment :image to t.has_attached_file :image in AddAttachmentImagetoPins.rb to try to resolve heroku migration error

--- db/migrate/20130214000817_add_attachment_image_to_pins.rb
+++ db/migrate/20130214000817_add_attachment_image_to_pins.rb
@@ -1,7 +1,7 @@
 class AddAttachmentImageToPins < ActiveRecord::Migration
   def self.up
     change_table :pins do |t|
-      t.column :attachment, :image
+      t.has_attached_file :image
     end
   end
 
