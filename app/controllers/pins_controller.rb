5034b0f7c22d8ace22d12fc9769c6332532d8940
liantics <liantics.antics@gmail.com>
2013-02-23 22:10:47 -0500

    pagination to 10, to test masonry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.order("created_at desc").paginate(:page => params[:page], :per_page =>5)
+    @pins = Pin.order("created_at desc").paginate(:page => params[:page], :per_page =>10)
     
 
     respond_to do |format|
