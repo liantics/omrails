eb559356b33214c3ff1a7575ec94232310b52b5b
liantics <liantics.antics@gmail.com>
2013-02-27 17:02:48 -0500

    precompiled assets manually

--- public/assets/active_admin/pages/application.js
+++ public/assets/active_admin/pages/application.js
@@ -1,1 +1,14 @@
-(function(){$(function(){return $(".datepicker").datepicker({dateFormat:"yy-mm-dd"}),$(".clear_filters_btn").click(function(){return window.location.search="",!1}),$(".dropdown_button").popover()})}).call(this);
+(function() {
+
+  $(function() {
+    $(".datepicker").datepicker({
+      dateFormat: "yy-mm-dd"
+    });
+    $(".clear_filters_btn").click(function() {
+      window.location.search = "";
+      return false;
+    });
+    return $(".dropdown_button").popover();
+  });
+
+}).call(this);
