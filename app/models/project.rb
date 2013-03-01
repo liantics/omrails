d3199b1acc2cbe8bb44beaced4cf5f40a614de6d
liantics <liantics.antics@gmail.com>
2013-03-01 15:53:36 -0500

    made project updates field conditional, made delete and edit avail only to project user

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ActiveRecord::Base
   attr_accessible :blurb, :category, :cost, :description, :progressupdate, :title
+
+	validates :user_id, presence: true
+
+  belongs_to :user
+
 end
