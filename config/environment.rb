1ba6798f331d3d8164413b9ac8b7800a7123a077
liantics <liantics.antics@gmail.com>
2013-03-19 18:09:05 -0400

    another precompile attempt and suppress deprecation warnings on startup

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#Turn off heroku deprecation warnings on server startup
+ActiveSupport::Deprecation.silenced = true   
+
 # Initialize the rails application
 Omrails::Application.initialize!
